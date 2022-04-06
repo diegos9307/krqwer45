a2cab172be588861c0aeb7ea145b25a329c2560f
Diego A. Suaza R <diegos9307@gmail.com>
2022-04-06 00:55:11 -0500

    haciendo correcciones

--- src/App.js
+++ src/App.js
@@ -8,32 +8,53 @@ class App extends Component {
     this.state = {
       tasks: [
         { id: 1, name: "Sacar la ropa", done: false },
-        { id: 2, name: "Hacer la cama", done: true },
+        { id: 2, name: "Hacer la cama", done: false },
         { id: 3, name: "Leer un rato", done: false },
       ],
       newTask: "",
+      error: "",
     };
-    this.handleKeyUp = this.handleKeyUp.bind(this);
   }
-  handleKeyUp(e) {
-    this.setState({
-      newTask: e.target.value,
-    });
-    if (e.target.value === "" && e.key === "Enter") {
-      let $input = document.getElementById("new-task");
-      $input.className = "error";
+
+  handleSubmit(e) {
+    e.preventDefault();
+    if (this.state.newTask === "") {
+      this.setState({
+        error: "error",
+      });
     } else {
-      let $input = document.getElementById("new-task");
-      $input.removeAttribute("class");
-      if (e.key === "Enter") {
-        let $capture = document.querySelector(".todo"),
-          $create = document.createElement("li");
-        $create.textContent = `${this.state.newTask}`;
-        $capture.appendChild($create);
-        e.target.value = null;
-      }
+      let oldTask = this.state.tasks;
+      const newTas = {
+        id: Math.floor(Math.random() * 100),
+        name: `${this.state.newTask}`,
+        done: false,
+      };
+      this.setState({
+        tasks: [...oldTask, newTas],
+        newTask: "",
+        error: "",
+      });
     }
   }
+  // handleKeyUp(e) {
+  //   this.setState({
+  //     newTask: e.target.value,
+  //   });
+  //   if (e.target.value === "" && e.key === "Enter") {
+  //     let $input = document.getElementById("new-task");
+  //     $input.className = "error";
+  //   } else {
+  //     let $input = document.getElementById("new-task");
+  //     $input.removeAttribute("class");
+  //     if (e.key === "Enter") {
+  //       let $capture = document.querySelector(".todo"),
+  //         $create = document.createElement("li");
+  //       $create.textContent = `${this.state.newTask}`;
+  //       $capture.appendChild($create);
+  //       e.target.value = null;
+  //     }
+  //   }
+  // }
   render() {
     return (
       <div className="wrapper">
@@ -41,24 +62,36 @@ class App extends Component {
           <h3>Por hacer:</h3>
           <ul className="todo">
             {this.state.tasks.map((task, index) => (
-              <li key={task.id}>{task.name}</li>
+              <li
+                key={task.id}
+                className={task.done ? "done" : ""}
+                onClick={(e) => {
+                  this.setState({
+                    task: (task.done = true),
+                  });
+                }}
+              >
+                {task.name}
+              </li>
             ))}
           </ul>
-          <form onSubmit={this.handleSubmit}>
+          <form onSubmit={this.handleSubmit.bind(this)}>
             <input
               type="text"
               id="new-task"
               placeholder="Ingresa una tarea y oprime Enter"
-              defaultValue={this.state.newTask}
-              onKeyUp={this.handleKeyUp}
+              value={this.state.newTask}
+              className={this.state.error}
+              onChange={(e) => {
+                this.setState({
+                  newTask: e.target.value,
+                });
+              }}
             />
           </form>
         </div>
       </div>
     );
-  }
-  handleSubmit(e) {
-    e.preventDefault();
   }
 }
 
